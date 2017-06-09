d102c38b859379073febcbea19736d7847706140
victor <vtic22@gmail.com>
2017-06-09 12:55:56 +0200

    Añadidas pruebas de Verificacion Inspec

--- test/recipes/default_test.rb
+++ test/recipes/default_test.rb
@@ -8,11 +8,16 @@
 unless os.windows?
   describe user('root') do
     it { should exist }
-    skip 'This is an example test, replace with your own test.'
+#    skip 'This is an example test, replace with your own test.'
   end
 end
 
 describe port(80) do
   it { should_not be_listening }
-  skip 'This is an example test, replace with your own test.'
+#  skip 'This is an example test, replace with your own test.'
 end
+
+describe package ('tree') do
+  it { should be_installed }
+end
+
